9336fb631814a15f6faeada3f597d76c7fdc0e93
Elizabeth Marston <elmarsto@microsoft.com>
2020-12-22 22:18:12 -0800

    progress

--- src/mogrify.rs
+++ src/mogrify.rs
@@ -3,7 +3,7 @@ use super::tsmorph_sys::*;
 
 #[wasm_bindgen]
 pub fn trans(proj: &Project) -> String {
-    let source_file = proj.Project_getSourceFile("spar.d.ts");
+    let source_file = proj.Project_getSourceFile("sample.d.ts");
     let function_declaration = source_file.SourceFile_getFunction("hello");
     function_declaration.FunctionDeclaration_getBodyText()
 }
--- src/tsmorph_sys.rs
+++ src/tsmorph_sys.rs
@@ -1,5 +1,5 @@
 use wasm_bindgen::prelude::*;
-use js_sys::{ Object };
+use js_sys::{ Object, Array };
 
 
 #[wasm_bindgen]
@@ -10,12 +10,42 @@ pub extern "C" {
     pub type SourceFile;
     #[wasm_bindgen(extends = Object)]
     pub type FunctionDeclaration;
+    #[wasm_bindgen(extends = Object)]
+    pub type VariableStatement;
+    #[wasm_bindgen(extends = Object)]
+    pub type VariableDeclaration;
+    #[wasm_bindgen(extends = Object)]
+    pub type EnumDeclaration;
+    #[wasm_bindgen(extends = Object)]
+    pub type EnumMember;
+    #[wasm_bindgen(extends = Object)]
+    pub type Parameter;
 
     #[wasm_bindgen(method, js_class="Project", js_name = getSourceFile)]
     pub fn Project_getSourceFile(this: &Project, name: &str) -> SourceFile;
     #[wasm_bindgen(method, js_class="SourceFile", js_name = getFunction)]
     pub fn SourceFile_getFunction(this: &SourceFile, name: &str) -> FunctionDeclaration;
+    #[wasm_bindgen(method, js_class="SourceFile", js_name = getFunctions)]
+    pub fn SourceFile_getFunctions(this: &SourceFile) -> Array;
+    #[wasm_bindgen(method, js_class="SourceFile", js_name = getVariableStatements)]
+    pub fn SourceFile_getVariableStatements(this: &SourceFile) -> Array;
+    #[wasm_bindgen(method, js_class="SourceFile", js_name = getEnums)]
+    pub fn SourceFile_getEnums(this: &SourceFile) -> Array;
+    #[wasm_bindgen(method, js_class = "SourceFile", js_name = getInterfaces)]
+    pub fn SourceFile_getInterfaces(this: &SourceFile) -> Array;
+    #[wasm_bindgen(method, js_class = "SourceFile", js_name = getTypeAliases)]
+    pub fn SourceFile_getTypeAliases(this: &SourceFile) -> Array;
+    #[wasm_bindgen(method, js_class = "SourceFile", js_name = getClasses)]
+    pub fn SourceFile_getClasses(this: &SourceFile) -> Array;
+
+
+
     #[wasm_bindgen(method, js_class="FunctionDeclaration", js_name = getBodyText)]
     pub fn FunctionDeclaration_getBodyText(this: &FunctionDeclaration) -> String;
+    #[wasm_bindgen(method, js_class = "FunctionDeclaration", js_name = getParameters)]
+    pub fn FunctionDeclaration_getParameters(this: &FunctionDeclaration) -> Array;
+    #[wasm_bindgen(method, js_class = "FunctionDeclaration", js_name = getTypeParameters)]
+    pub fn FunctionDeclaration_getTypeParameters(this: &FunctionDeclaration) -> Array;
+
 }
 
